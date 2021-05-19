e59e0b2e72fc133f9d1098f77db3594a5e5f8819
Mateo-Danton <72432264+Mateo-Danton@users.noreply.github.com>
2021-05-19 19:26:15 +0200

    mise a jour site web
    màj web avec grille qui fonctionne, page d'accueil revue, et quelques nombreux ajustements.

--- game.php
+++ game.php
@@ -2,142 +2,165 @@
 <main>
     <h1>Jouer à Anisen Crossing</h1>
     <table>
-        <tr>
+      <tr class="legendC">
+        <td class="null"></td>
+        <td>6 3</td>
+        <td>1 8</td>
+        <td>1 1 1 2</td>
+        <td>2 3</td>
+        <td>5</td>
+        <td>7 1</td>
+        <td>4 5</td>
+        <td>1 1 1 1 1 1 1</td>
+        <td>2</td>
+        <td>5 3</td>
+      </tr>
+      <tr>
+        <td class="legendL">1 1 1 1</td>
         <td class="dot"></td>
-            <td class="dot"></td>
-            <td class="dot"></td>
-            <td class="dot"></td>
-            <td class="dot"></td>
-            <td class="dot"></td>
-            <td class="dot"></td>
-            <td class="dot"></td>
-            <td class="dot"></td>
-            <td class="dot"></td>
-        </tr>
-        <tr>
         <td class="dot"></td>
-            <td class="dot"></td>
-            <td class="dot"></td>
-            <td class="dot"></td>
-            <td class="dot"></td>
-            <td class="dot"></td>
-            <td class="dot"></td>
-            <td class="dot"></td>
-            <td class="dot"></td>
-            <td class="dot"></td>
-        </tr>
-        <tr>
         <td class="dot"></td>
-            <td class="dot"></td>
-            <td class="dot"></td>
-            <td class="dot"></td>
-            <td class="dot"></td>
-            <td class="dot"></td>
-            <td class="dot"></td>
-            <td class="dot"></td>
-            <td class="dot"></td>
-            <td class="dot"></td>
-        </tr>
-        <tr>
         <td class="dot"></td>
-            <td class="dot"></td>
-            <td class="dot"></td>
-            <td class="dot"></td>
-            <td class="dot"></td>
-            <td class="dot"></td>
-            <td class="dot"></td>
-            <td class="dot"></td>
-            <td class="dot"></td>
-            <td class="dot"></td>
-        </tr>
-        <tr>
         <td class="dot"></td>
-            <td class="dot"></td>
-            <td class="dot"></td>
-            <td class="dot"></td>
-            <td class="dot"></td>
-            <td class="dot"></td>
-            <td class="dot"></td>
-            <td class="dot"></td>
-            <td class="dot"></td>
-            <td  class="dot"></td>
-        </tr>
-        <tr>
-            <td class="dot"></td>
-            <td class="dot"></td>
-            <td class="dot"></td>
-            <td class="dot"></td>
-            <td class="dot"></td>
-            <td class="dot"></td>
-            <td class="dot"></td>
-            <td class="dot"></td>
-            <td class="dot"></td>
-            <td class="dot"></td>
-        </tr>
-        <tr>
-            <td class="dot"></td>
-            <td class="dot"></td>
-            <td class="dot"></td>
-            <td class="dot"></td>
-            <td class="dot"></td>
-            <td class="dot"></td>
-            <td class="dot"></td>
-            <td class="dot"></td>
-            <td class="dot"></td>
-            <td class="dot"></td>
-        </tr>
-        <tr>
-            <td class="dot"></td>
-            <td class="dot"></td>
-            <td class="dot"></td>
-            <td class="dot"></td>
-            <td class="dot"></td>
-            <td class="dot"></td>
-            <td class="dot"></td>
-            <td class="dot"></td>
-            <td class="dot"></td>
-            <td class="dot"></td>
-        </tr>
-        <tr>
-            <td class="dot"></td>
-            <td class="dot"></td>
-            <td class="dot"></td>
-            <td class="dot"></td>
-            <td class="dot"></td>
-            <td class="dot"></td>
-            <td class="dot"></td>
-            <td class="dot"></td>
-            <td class="dot"></td>
-            <td class="dot"></td>
-        </tr>
-        <tr>
-            <td class="dot"></td>
-            <td class="dot"></td>
-            <td class="dot"></td>
-            <td class="dot"></td>
-            <td class="dot"></td>
-            <td class="dot"></td>
-            <td class="dot"></td>
-            <td class="dot"></td>
-            <td class="dot"></td>
-            <td class="dot"></td>
-        </tr>
-    </table>
+        <td class="dot"></td>
+        <td class="dot"></td>
+        <td class="dot"></td>
+        <td class="dot"></td>
+        <td class="dot"></td>
+      </tr>
+      <tr>
+        <td class="legendL">3 5</td>
+        <td class="dot"></td>
+        <td class="dot"></td>
+        <td class="dot"></td>
+        <td class="dot"></td>
+        <td class="dot"></td>
+        <td class="dot"></td>
+        <td class="dot"></td>
+        <td class="dot"></td>
+        <td class="dot"></td>
+        <td class="dot"></td>
+      </tr>
+      <tr>
+        <td class="legendL">2 7</td>
+        <td class="dot"></td>
+        <td class="dot"></td>
+        <td class="dot"></td>
+        <td class="dot"></td>
+        <td class="dot"></td>
+        <td class="dot"></td>
+        <td class="dot"></td>
+        <td class="dot"></td>
+        <td class="dot"></td>
+        <td class="dot"></td>
+      </tr>
+      <tr>
+        <td class="legendL">3</td>
+        <td class="dot"></td>
+        <td class="dot"></td>
+        <td class="dot"></td>
+        <td class="dot"></td>
+        <td class="dot"></td>
+        <td class="dot"></td>
+        <td class="dot"></td>
+        <td class="dot"></td>
+        <td class="dot"></td>
+        <td class="dot"></td>
+      </tr>
+      <tr>
+        <td class="legendL">1 1 1 2 3</td>
+        <td class="dot"></td>
+        <td class="dot"></td>
+        <td class="dot"></td>
+        <td class="dot"></td>
+        <td class="dot"></td>
+        <td class="dot"></td>
+        <td class="dot"></td>
+        <td class="dot"></td>
+        <td class="dot"></td>
+        <td  class="dot"></td>
+      </tr>
+      <tr>
+      <td class="legendL"></td>
+        <td class="dot"></td>
+        <td class="dot"></td>
+        <td class="dot"></td>
+        <td class="dot"></td>
+        <td class="dot"></td>
+        <td class="dot"></td>
+        <td class="dot"></td>
+        <td class="dot"></td>
+        <td class="dot"></td>
+        <td class="dot"></td>
+      </tr>
+      <tr>
+      <td class="legendL"></td>
+        <td class="dot"></td>
+        <td class="dot"></td>
+        <td class="dot"></td>
+        <td class="dot"></td>
+        <td class="dot"></td>
+        <td class="dot"></td>
+        <td class="dot"></td>
+        <td class="dot"></td>
+        <td class="dot"></td>
+        <td class="dot"></td>
+      </tr>
+      <tr>
+      <td class="legendL"></td>
+        <td class="dot"></td>
+        <td class="dot"></td>
+        <td class="dot"></td>
+        <td class="dot"></td>
+        <td class="dot"></td>
+        <td class="dot"></td>
+        <td class="dot"></td>
+        <td class="dot"></td>
+        <td class="dot"></td>
+        <td class="dot"></td>
+      </tr>
+      <tr>
+      <td class="legendL"></td>
+        <td class="dot"></td>
+        <td class="dot"></td>
+        <td class="dot"></td>
+        <td class="dot"></td>
+        <td class="dot"></td>
+        <td class="dot"></td>
+        <td class="dot"></td>
+        <td class="dot"></td>
+        <td class="dot"></td>
+        <td class="dot"></td>
+      </tr>
+      <tr>
+      <td class="legendL"></td>
+        <td class="dot"></td>
+        <td class="dot"></td>
+        <td class="dot"></td>
+        <td class="dot"></td>
+        <td class="dot"></td>
+        <td class="dot"></td>
+        <td class="dot"></td>
+        <td class="dot"></td>
+        <td class="dot"></td>
+        <td class="dot"></td>
+      </tr>
+  </table>
 </div>
 
 <script>
-  const buttons = document.querySelectorAll('td');
+  const buttons = document.querySelectorAll('td.dot');
 
 for(let i = 0; i < buttons.length; i++){
     buttons[i].addEventListener('click', CarreNoir);
 }
 
 function CarreNoir(){
-    if (this.style.backgroundColor !== "black" && this.style.backgroundColor !== "green" ) {
+    if (this.style.backgroundColor !== "black" && this.style.backgroundColor !== "#11ffee00" ) {
    this.style.backgroundColor = "black"; 
     }
-    else if (this.style.backgroundColor !== "#ccc" && this.style.backgroundColor !== "green") {
-        this.style.backgroundColor = "green";
+    else if (this.style.backgroundColor !== "#ccc" && this.style.backgroundColor !== "#11ffee00") {
+        this.style.backgroundColor = "#11ffee00";
     }
     else {
         this.style.backgroundColor = "#ccc";
--- index.php
+++ index.php
@@ -1,9 +1,24 @@
+<?php require 'includes /header.php'; ?>
+<main>
+    <h1>Anisen Crossing</h1>
+    <h2>Take a break and play one of the most famous games in the world:<br>Nonogram !</h2>
 
-    <?php
-     require 'includes /header.php'; 
-     ?>
-    <main>
-    <p>Hello world!</p>
-    <a href="game.php">jeu</a>
-    </main>
-    <?php require 'includes /footer.php'; ?>
+    <section class="story">
+    <h3><u>Some informations on Nonograms</u></h3>
+    <p>Nonograms are a type of puzzles on a grid, derivated from another famous grid puzzle: crosswords. Early Crosswords often used pictures as part of their layout: an example is Stephenson's Train, published in Italy in 1925. However nonograms are fairly newer than crosswords, as their origin is found in 1987, in Tokyo.</p>
+
+    <p>Their creator, Graphics editor Non Ishida, got the idea after winning a competition to design a picture created by having certain lights on or off in a skyscraper: a puzzle based around filling in certain squares in a grid. The following year, she published only three picture grid puzzles in Japan using the name "Window Art Puzzles", and coincidentally Mr Tetsuya Nishio completely independently also published this type of puzzle in a different magazine. In 1989, James Dalgety discovers this new puzzle form and make an arrangement with Non Ishida to commercialise her designs throughout the world outside Japan, inventing the name Nonogram after "Non" Ishida and Dia"gram".</p>
+
+    <p>They were an immediate success and are still widely spread today.</p>
+    </section>
+
+    <button class="tuto"type="button">
+        <a href="tutorial.php">Tutorial</a>
+    </button>
+
+    <button class="game"type="button">
+        <a href="game.php">Play the Game!</a>
+    </button>
+    
+</main>
+<?php require 'includes /footer.php'; ?>
