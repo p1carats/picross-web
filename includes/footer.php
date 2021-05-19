44266a615761de4d9306575368cf824c96a07aae
Mateo-Danton <72432264+Mateo-Danton@users.noreply.github.com>
2021-05-19 19:26:58 +0200

    màj
    màj footer et header en lien avec les pages du site

--- includes/footer.php
+++ includes/footer.php
@@ -1,4 +1,5 @@
 <footer>
+    <hr>
     <p>Copyright &copy; 2021 - ISEN Lille</p>
     <p>Groupe 6 - Xx_D4rknès_SlayerZ</p>
 </footer>
--- includes/header.php
+++ includes/header.php
@@ -8,6 +8,9 @@
   <link rel="stylesheet" href="style.css">
   <!-- Font Awesome -->
   <script src="https://kit.fontawesome.com/7d213dfca0.js" crossorigin="anonymous"></script>
+  <!-- Google Font -->
+  <link rel="preconnect" href="https://fonts.gstatic.com">
+  <link href="https://fonts.googleapis.com/css2?family=Sanchez&display=swap" rel="stylesheet">  
   <title>Anisen Crossing</title>
 </head>
 
@@ -16,11 +19,12 @@
   <div id="myNav" class="overlay">
     <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
     <div class="overlay-content">
+      <h1>Anisen Crossing</h1>
       <a href="index.php">Home</a>
-      <p>Réalisé par les membres de l'équipe Xx_D4rknès_SlayerZ: <br>Romain Altmayer, Matéo Danton - Design Web<br>Cyril Cuvelier, Noé Barlet - Game Programmer<br>Achraf Laalou, Simon Losson - Game Programmer, the return</p>
+      <p>Created by the members of the <u>Xx_D4rknès_SlayerZ</u> crew: <br>Romain Altmayer, Matéo Danton - Web Design<br>Cyril Cuvelier, Noé Barlet - Game Programmer<br>Achraf Laalou, Simon Losson - Game Programmer</p>
     </div>
   </div>
-<span style="font-size:30px;cursor:pointer" onclick="openNav()">&#9776; Anisen Crossing</span>
+<span style="font-size:30px;cursor:pointer" onclick="openNav()">&#9776;</span>
 
 <script type="text/javascript">
 function openNav() {
