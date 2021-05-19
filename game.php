e40d9ae5c5caf8070eeec778848c045b7d50acf1
Ommellete <72408158+Ommellete@users.noreply.github.com>
2021-05-19 10:40:00 +0200

    Add files via upload

--- game.php
+++ game.php
@@ -3,137 +3,145 @@
     <h1>Jouer à Anisen Crossing</h1>
     <table>
         <tr>
-        <td   class="dot"></td>
-            <td   class="dot"></td>
-            <td   class="dot"></td>
-            <td   class="dot"></td>
-            <td   class="dot"></td>
-            <td   class="dot"></td>
-            <td   class="dot"></td>
-            <td   class="dot"></td>
-            <td   class="dot"></td>
-            <td   class="dot"></td>
+        <td class="dot"></td>
+            <td class="dot"></td>
+            <td class="dot"></td>
+            <td class="dot"></td>
+            <td class="dot"></td>
+            <td class="dot"></td>
+            <td class="dot"></td>
+            <td class="dot"></td>
+            <td class="dot"></td>
+            <td class="dot"></td>
         </tr>
         <tr>
-        <td   class="dot"></td>
-            <td   class="dot"></td>
-            <td   class="dot"></td>
-            <td   class="dot"></td>
-            <td   class="dot"></td>
-            <td   class="dot"></td>
-            <td   class="dot"></td>
-            <td   class="dot"></td>
-            <td   class="dot"></td>
-            <td   class="dot"></td>
+        <td class="dot"></td>
+            <td class="dot"></td>
+            <td class="dot"></td>
+            <td class="dot"></td>
+            <td class="dot"></td>
+            <td class="dot"></td>
+            <td class="dot"></td>
+            <td class="dot"></td>
+            <td class="dot"></td>
+            <td class="dot"></td>
         </tr>
         <tr>
-        <td   class="dot"></td>
-            <td   class="dot"></td>
-            <td   class="dot"></td>
-            <td   class="dot"></td>
-            <td   class="dot"></td>
-            <td   class="dot"></td>
-            <td   class="dot"></td>
-            <td   class="dot"></td>
-            <td   class="dot"></td>
-            <td   class="dot"></td>
+        <td class="dot"></td>
+            <td class="dot"></td>
+            <td class="dot"></td>
+            <td class="dot"></td>
+            <td class="dot"></td>
+            <td class="dot"></td>
+            <td class="dot"></td>
+            <td class="dot"></td>
+            <td class="dot"></td>
+            <td class="dot"></td>
         </tr>
         <tr>
-        <td   class="dot"></td>
-            <td   class="dot"></td>
-            <td   class="dot"></td>
-            <td   class="dot"></td>
-            <td   class="dot"></td>
-            <td   class="dot"></td>
-            <td   class="dot"></td>
-            <td   class="dot"></td>
-            <td   class="dot"></td>
-            <td   class="dot"></td>
+        <td class="dot"></td>
+            <td class="dot"></td>
+            <td class="dot"></td>
+            <td class="dot"></td>
+            <td class="dot"></td>
+            <td class="dot"></td>
+            <td class="dot"></td>
+            <td class="dot"></td>
+            <td class="dot"></td>
+            <td class="dot"></td>
         </tr>
         <tr>
-        <td   class="dot"></td>
-            <td   class="dot"></td>
-            <td   class="dot"></td>
-            <td   class="dot"></td>
-            <td   class="dot"></td>
-            <td   class="dot"></td>
-            <td   class="dot"></td>
-            <td   class="dot"></td>
-            <td   class="dot"></td>
-            <td   class="dot"></td>
+        <td class="dot"></td>
+            <td class="dot"></td>
+            <td class="dot"></td>
+            <td class="dot"></td>
+            <td class="dot"></td>
+            <td class="dot"></td>
+            <td class="dot"></td>
+            <td class="dot"></td>
+            <td class="dot"></td>
+            <td  class="dot"></td>
         </tr>
         <tr>
-            <td   class="dot"></td>
-            <td   class="dot"></td>
-            <td   class="dot"></td>
-            <td   class="dot"></td>
-            <td   class="dot"></td>
-            <td   class="dot"></td>
-            <td   class="dot"></td>
-            <td   class="dot"></td>
-            <td   class="dot"></td>
-            <td   class="dot"></td>
+            <td class="dot"></td>
+            <td class="dot"></td>
+            <td class="dot"></td>
+            <td class="dot"></td>
+            <td class="dot"></td>
+            <td class="dot"></td>
+            <td class="dot"></td>
+            <td class="dot"></td>
+            <td class="dot"></td>
+            <td class="dot"></td>
         </tr>
         <tr>
-            <td   class="dot"></td>
-            <td   class="dot"></td>
-            <td   class="dot"></td>
-            <td   class="dot"></td>
-            <td   class="dot"></td>
-            <td   class="dot"></td>
-            <td   class="dot"></td>
-            <td   class="dot"></td>
-            <td   class="dot"></td>
-            <td   class="dot"></td>
+            <td class="dot"></td>
+            <td class="dot"></td>
+            <td class="dot"></td>
+            <td class="dot"></td>
+            <td class="dot"></td>
+            <td class="dot"></td>
+            <td class="dot"></td>
+            <td class="dot"></td>
+            <td class="dot"></td>
+            <td class="dot"></td>
         </tr>
         <tr>
-            <td   class="dot"></td>
-            <td   class="dot"></td>
-            <td   class="dot"></td>
-            <td   class="dot"></td>
-            <td   class="dot"></td>
-            <td   class="dot"></td>
-            <td   class="dot"></td>
-            <td   class="dot"></td>
-            <td   class="dot"></td>
-            <td   class="dot"></td>
+            <td class="dot"></td>
+            <td class="dot"></td>
+            <td class="dot"></td>
+            <td class="dot"></td>
+            <td class="dot"></td>
+            <td class="dot"></td>
+            <td class="dot"></td>
+            <td class="dot"></td>
+            <td class="dot"></td>
+            <td class="dot"></td>
         </tr>
         <tr>
-            <td   class="dot"></td>
-            <td   class="dot"></td>
-            <td   class="dot"></td>
-            <td   class="dot"></td>
-            <td   class="dot"></td>
-            <td   class="dot"></td>
-            <td   class="dot"></td>
-            <td   class="dot"></td>
-            <td   class="dot"></td>
-            <td   class="dot"></td>
+            <td class="dot"></td>
+            <td class="dot"></td>
+            <td class="dot"></td>
+            <td class="dot"></td>
+            <td class="dot"></td>
+            <td class="dot"></td>
+            <td class="dot"></td>
+            <td class="dot"></td>
+            <td class="dot"></td>
+            <td class="dot"></td>
         </tr>
         <tr>
-            <td   class="dot"></td>
-            <td   class="dot"></td>
-            <td   class="dot"></td>
-            <td   class="dot"></td>
-            <td   class="dot"></td>
-            <td   class="dot"></td>
-            <td   class="dot"></td>
-            <td   class="dot"></td>
-            <td   class="dot"></td>
-            <td   class="dot"></td>
+            <td class="dot"></td>
+            <td class="dot"></td>
+            <td class="dot"></td>
+            <td class="dot"></td>
+            <td class="dot"></td>
+            <td class="dot"></td>
+            <td class="dot"></td>
+            <td class="dot"></td>
+            <td class="dot"></td>
+            <td class="dot"></td>
         </tr>
     </table>
 </div>
 
 <script>
-  const buttons = document.querySelectorAll('dot');
+  const buttons = document.querySelectorAll('td');
 
 for(let i = 0; i < buttons.length; i++){
     buttons[i].addEventListener('click', CarreNoir);
 }
 
 function CarreNoir(){
-   document.dot.style.backgroundColor = black; 
+    if (this.style.backgroundColor !== "black" && this.style.backgroundColor !== "green" ) {
+   this.style.backgroundColor = "black"; 
+    }
+    else if (this.style.backgroundColor !== "#ccc" && this.style.backgroundColor !== "green") {
+        this.style.backgroundColor = "green";
+    }
+    else {
+        this.style.backgroundColor = "#ccc";
+    }
 }
 </script>
 </main>
