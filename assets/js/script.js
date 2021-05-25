a555a4527e188d59d0d20adeea57ba088f45a9ff
Mateo-Danton <72432264+Mateo-Danton@users.noreply.github.com>
2021-05-25 11:43:54 +0200

    update js

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,19 +6,23 @@ function closeNav() {
   document.getElementById("navbar").style.height = "0%";
 }
 
-// game
-const buttons = document.querySelectorAll('td.dot');
-
-for(let i = 0; i < buttons.length; i++) {
-  buttons[i].addEventListener('click', CarreNoir);
-}
-
-function CarreNoir() {
-  if (this.style.backgroundColor !== "black" && this.style.backgroundColor !== "#11ffee00" ) {
-    this.style.backgroundColor = "black"; 
-  } else if (this.style.backgroundColor !== "#ccc" && this.style.backgroundColor !== "#11ffee00") {
-    this.style.backgroundColor = "#11ffee00";
-  } else {
-    this.style.backgroundColor = "#ccc";
-  }
+/// chrono
+ss=0;
+s=00;
+m='0'+0;
+h='0'+0;
+window.status = "Chrono " +h + ' : '  + m + ' : ' + s ; 
+function Chrono() {
+if (m==0) {m='00'}
+if(h==0) {h='00'}
+if (ss<10){ss='0'+ss;}
+if (s<10){s='0'+s;}
+window.status = "Chrono " +h + ' : '  + m + ' : ' + s ; 
+document.formu.heure.value =+h + ' : ' + m + ' : ' + s ;
+s++;
+ss++;
+if (h==24){h='0'+0;}
+if (s==60){s=0;s=0; m++;if(m<10){m='0'+m;}}
+if (m==60){m='0'+0;h++;if(h<10){h='0'+h;}}
+chrono=window.setTimeout("Chrono();",1000);
 }
