05d04eb3f935b67f60e7ba4476bb54772f3a8ab4
Mateo-Danton <72432264+Mateo-Danton@users.noreply.github.com>
2021-05-26 09:13:49 +0200

    Add files via upload

--- includes/header.php
+++ includes/header.php
@@ -4,7 +4,7 @@
     <div class="overlay-content">
       <h1>Anisen Crossing</h1>
       <a href="index.php">Home</a>
-      <p>Created by the members of the <u>Xx_D4rknès_SlayerZ</u> crew: <br>Romain Altmayer, Matéo Danton - Web Design<br>Cyril Cuvelier, Noé Barlet - Game Programmer<br>Achraf Laalou, Simon Losson - Game Programmer</p>
+      <p>Created by the members of the <u>Xx_D4rknès_SlayerZ</u> crew: <br>Romain Altmayer, Matéo Danton - Web Design<br>Cyril Cuvelier, Noé Barlet - Game Programmer<br>Achraf Laalou - Game Programmer<br>Simon Losson - Happiness Manager</p>
     </div>
   </div>
   <span style="font-size:30px;cursor:pointer" onclick="openNav()">&#9776;</span>
