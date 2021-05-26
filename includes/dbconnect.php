4e1fdd310a6a934bfa34ebfa1e0f015033a6fc25
Mateo-Danton <72432264+Mateo-Danton@users.noreply.github.com>
2021-05-26 17:43:39 +0200

    added dbconnect /!\ NO DATABASE
    added dbconnect, which is essentially the file to connect the website to a database. /!\ IMPORTANT /!\ The database needs to be created on your side for now, until a proper solution too have a shared database is found.

---
+++ includes/dbconnect.php
@@ -0,0 +1,15 @@
+<?php
+$hostname = 'localhost';
+$dbname = 'picross';
+$dbuser = 'root';
+$dbpass = '';
+try
+{
+  $bdd = new PDO("mysql:host=$hostname;dbname=$dbname;charset=utf8", $dbuser, $dbpass, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+}
+catch (Exception $e)
+{
+  die('Erreur : ' . $e->getMessage());
+}
+//echo 'Connexion BDD réussie';
+?>
