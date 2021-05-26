6c73f6115b84fea92ed51aae02d4aedbb20e90ae
Mateo-Danton <72432264+Mateo-Danton@users.noreply.github.com>
2021-05-26 09:36:57 +0200

    Add files via upload

--- views/error.php
+++ views/error.php
@@ -1,4 +1,7 @@
 <div id="error">
-  <p>Error 404 - Not Found</p>
+  <h3>Error 404 - Not Found</h3>
+  <p>Uh oh! Something went wrong - You Picrossed into the wrong neighborhood!</p>
+  <p>Quickly get out of here before Resetti catches you!</p>
+  <img src="assets/img/resettiError.png" alt="Angry Resetti"></img>
   <!-- TO-DO -->
 </div>
