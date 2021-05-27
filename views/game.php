bd55b86a0ff75a7c3c987dee920f5d24374d8272
Mateo-Danton <72432264+Mateo-Danton@users.noreply.github.com>
2021-05-27 14:35:53 +0200

    Updated web pages and picross test file
    Updated pages in order to work with the database and correct an error in the picross test text file.
    As before, the database is still done on a personal server, you need to make one in order to make the website work as intended.

--- views/game.php
+++ views/game.php
@@ -1,3 +1,15 @@
+<?php 
+  $req = $bdd->prepare('SELECT LegendC, LegendL, solution FROM grid');
+  // Execution de la requête
+  $req->execute();
+  // Recuperation du resultat;
+  $data = $req->fetchAll();
+
+  foreach($data as $datarow) {
+    $grid = $datarow['solution'];
+  }
+?>
+
 <div id="game">
   <h1>Play Anisen Crossing</h1>
   <button type="button" class="themeBtton" onclick="day()">Day Theme</button>
@@ -8,35 +20,22 @@
       <input name="heure" type="text" id="heure" value="00 : 00 : 00">
     </p>
   </form>
-  <table>
+  <table id="grid">
     <tr class="legendC">
       <td class="null"></td>
-      <td><div class="box">3</div><div class="box">2</div><div class="box"></div><div class="box"></div><div class="box"></div></td>
-      <td><div class="box">1</div><div class="box">2</div><div class="box">1</div><div class="box">1</div><div class="box"></div></td>
-      <td><div class="box">1</div><div class="box">2</div><div class="box">1</div><div class="box">2</div><div class="box"></div></td>
-      <td><div class="box">1</div><div class="box">2</div><div class="box">4</div><div class="box"></div><div class="box"></div></td>
-      <td><div class="box">2</div><div class="box">1</div><div class="box">2</div><div class="box"></div><div class="box"></div></td>
-      <td><div class="box">1</div><div class="box">4</div><div class="box"></div><div class="box"></div><div class="box"></div></td>
-      <td><div class="box">1</div><div class="box">2</div><div class="box">3</div><div class="box"></div><div class="box"></div></td>
-      <td><div class="box">1</div><div class="box">4</div><div class="box">1</div><div class="box"></div><div class="box"></div></td>
-      <td><div class="box">2</div><div class="box">3</div><div class="box">3</div><div class="box"></div><div class="box"></div></td>
-      <td><div class="box">3</div><div class="box">3</div><div class="box"></div><div class="box"></div><div class="box"></div></td>
+      <td><div class="box">4</div><div class="box">3</div><div class="box"></div><div class="box"></div><div class="box"></div></td>
+      <td><div class="box">2</div><div class="box">3</div><div class="box"></div><div class="box"></div><div class="box"></div></td>
+      <td><div class="box">3</div><div class="box">1</div><div class="box">3</div><div class="box"></div><div class="box"></div></td>
+      <td><div class="box">1</div><div class="box">3</div><div class="box"></div><div class="box"></div><div class="box"></div></td>
+      <td><div class="box">2</div><div class="box">3</div><div class="box"></div><div class="box"></div><div class="box"></div></td>
+      <td><div class="box">2</div><div class="box">1</div><div class="box">3</div><div class="box"></div><div class="box"></div></td>
+      <td><div class="box">3</div><div class="box">1</div><div class="box">2</div><div class="box"></div><div class="box"></div></td>
+      <td><div class="box">2</div><div class="box">2</div><div class="box"></div><div class="box"></div><div class="box"></div></td>
+      <td><div class="box">2</div><div class="box">2</div><div class="box"></div><div class="box"></div><div class="box"></div></td>
+      <td><div class="box">3</div><div class="box"></div><div class="box"></div><div class="box"></div><div class="box"></div></td>
     </tr>
     <tr>
-      <td class="legendL">3 3</td>
-      <td class="dot"></td>
-      <td class="dot"></td>
-      <td class="dot"></td>
-      <td class="dot"></td>
-      <td class="dot"></td>
-      <td class="dot"></td>
-      <td class="dot"></td>
-      <td class="dot"></td>
-      <td class="dot"></td>
-      <td class="dot"></td>
-    </tr>
-    <tr>
-      <td class="legendL">1 2</td>
+      <td class="legendL">8</td>
       <td class="dot"></td>
       <td class="dot"></td>
       <td class="dot"></td>
@@ -49,7 +48,7 @@
       <td class="dot"></td>
     </tr>
     <tr>
-      <td class="legendL">5 1</td>
+      <td class="legendL">1 6</td>
       <td class="dot"></td>
       <td class="dot"></td>
       <td class="dot"></td>
@@ -62,7 +61,7 @@
       <td class="dot"></td>
     </tr>
     <tr>
-      <td class="legendL">10</td>
+      <td class="legendL">3 1 1</td>
       <td class="dot"></td>
       <td class="dot"></td>
       <td class="dot"></td>
@@ -75,7 +74,20 @@
       <td class="dot"></td>
     </tr>
     <tr>
-      <td class="legendL">3</td>
+      <td class="legendL">2</td>
+      <td class="dot"></td>
+      <td class="dot"></td>
+      <td class="dot"></td>
+      <td class="dot"></td>
+      <td class="dot"></td>
+      <td class="dot"></td>
+      <td class="dot"></td>
+      <td class="dot"></td>
+      <td class="dot"></td>
+      <td class="dot"></td>
+    </tr>
+    <tr>
+      <td class="legendL">1</td>
       <td class="dot"></td>
       <td class="dot"></td>
       <td class="dot"></td>
@@ -88,7 +100,7 @@
       <td  class="dot"></td>
     </tr>
     <tr>
-      <td class="legendL">1 2</td>
+      <td class="legendL">1 1</td>
       <td class="dot"></td>
       <td class="dot"></td>
       <td class="dot"></td>
@@ -101,7 +113,7 @@
       <td class="dot"></td>
     </tr>
     <tr>
-      <td class="legendL">8 1</td>
+      <td class="legendL">2</td>
       <td class="dot"></td>
       <td class="dot"></td>
       <td class="dot"></td>
@@ -114,7 +126,7 @@
       <td class="dot"></td>
     </tr>
     <tr>
-      <td class="legendL">1 2 2</td>
+      <td class="legendL">5</td>
       <td class="dot"></td>
       <td class="dot"></td>
       <td class="dot"></td>
@@ -127,7 +139,7 @@
       <td class="dot"></td>
     </tr>
     <tr>
-    <td class="legendL">5 2</td>
+    <td class="legendL">9</td>
       <td class="dot"></td>
       <td class="dot"></td>
       <td class="dot"></td>
@@ -140,7 +152,7 @@
       <td class="dot"></td>
     </tr>
     <tr>
-      <td class="legendL">5 2</td>
+      <td class="legendL">9</td>
       <td class="dot"></td>
       <td class="dot"></td>
       <td class="dot"></td>
@@ -155,25 +167,54 @@
   </table>
   <button class="solveBtton"type="button">
         <a href="#">Solve for me!</a>
-    </button>
+  </button>
 </div>
 <script>
   const buttons = document.querySelectorAll('td.dot');
 
 for(let i = 0; i < buttons.length; i++){
     buttons[i].addEventListener('click', CarreNoir);
+    buttons[i].addEventListener('click', TableData);
 }
 
 function CarreNoir(){
     if (this.style.backgroundColor !== "black" && this.style.backgroundColor !== "#11ffee00" ) {
    this.style.backgroundColor = "black"; 
+   this.innerHTML = "1";
+   this.style.color = "transparent";
     }
     else if (this.style.backgroundColor !== "#ccc" && this.style.backgroundColor !== "#11ffee00") {
         this.style.backgroundColor = "#11ffee00";
+        this.innerHTML = "0";
+        this.style.color = "transparent";
     }
     else {
         this.style.backgroundColor = "#ccc";
+        this.innerHTML = "0";
+        this.style.color = "transparent";
     }
+}
+
+function TableData() {
+  var infoGrid = "";
+  var myTab = document.getElementById('grid');
+
+  // LOOP THROUGH EACH ROW OF THE TABLE AFTER HEADER.
+  for (i = 1; i < myTab.rows.length; i++) {
+
+    // GET THE CELLS COLLECTION OF THE CURRENT ROW.
+    var objCells = myTab.rows.item(i).cells;
+
+    // LOOP THROUGH EACH CELL OF THE CURENT ROW TO READ CELL VALUES.
+    for (var j = 1; j < objCells.length; j++) {
+      infoGrid = infoGrid + objCells.item(j).innerHTML;
+    }
+  }
+
+  if(infoGrid == "<?php echo $grid ?>"){
+    alert("Game won!");
+    window.clearTimeout(chrono);
+  }
 }
 
 window.onload = Chrono();
