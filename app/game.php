eab01c08a3fca7f7665f53e22a0a5ab98cd3f958
Mateo-Danton <72432264+Mateo-Danton@users.noreply.github.com>
2021-05-31 16:38:39 +0200

    update php code for pages

--- app/game.php
+++ app/game.php
@@ -1,15 +1,19 @@
 <?php 
 // Getting data of the game back from the database
 // Preparing the selection
-$req = $bdd->prepare('SELECT legendC, legendL, solution FROM grid');
+$req = $bdd->prepare('SELECT dimension, legendC, legendL, solution FROM grid');
 // Execution
 $req->execute();
 // Recovery of the data
 $data = $req->fetchAll();
 // Separation of the different datas needed
 foreach($data as $datarow) {
+  $dimension = $datarow['dimension'];
   $legendC = $datarow['legendC'];
   $legendL = $datarow['legendL'];
   $grid = $datarow['solution'];
 }
+
+$legendsC = explode(";", $legendC);
+$legendsL = explode(";", $legendL);
 ?>
--- app/scoreboard.php
+++ app/scoreboard.php
@@ -1,7 +1,20 @@
 <?php 
-// Preparing the selection
-$req = $bdd->prepare('SELECT nom, temps FROM scoreboard10a ORDER BY temps');
+    require "game.php";
+    switch($dimension){
+    case "10":
+        // Preparing the selection
+        $req = $bdd->prepare('SELECT nom, temps FROM scoreboard10a ORDER BY temps');
+        break;
+    case "15":
+        // Preparing the selection
+        $req = $bdd->prepare('SELECT nom, temps FROM scoreboard15a ORDER BY temps');
+        break;
+    case "20":
+        // Preparing the selection
+        $req = $bdd->prepare('SELECT nom, temps FROM scoreboard20a ORDER BY temps');
+        break;
+    }
 // Execution
 $req->execute();
 // Recovery of the data
-$data = $req->fetchAll(); 
+$data = $req->fetchAll(); ?>
