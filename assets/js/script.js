bff22e8933644003b7c6d86bf807934f729fa4b4
Mateo-Danton <72432264+Mateo-Danton@users.noreply.github.com>
2021-05-31 16:36:10 +0200

    update js

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,17 +11,18 @@ ss=0;
 s=00;
 m='0'+0;
 h='0'+0;
+j=0;
 window.status = "Chrono " +h + ' : '  + m + ' : ' + s ; 
 function Chrono() {
 if (m==0) {m='00'}
 if(h==0) {h='00'}
 if (ss<10){ss='0'+ss;}
 if (s<10){s='0'+s;}
-window.status = "Chrono " +h + ' : '  + m + ' : ' + s ; 
+ window.status = "Chrono " +h + ' : '  + m + ' : ' + s ; 
 document.formu.heure.value =+h + ' : ' + m + ' : ' + s ;
+document.formu.seco.value =ss;
 s++;
 ss++;
-if (h==24){h='0'+0;}
 if (s==60){s=0;s=0; m++;if(m<10){m='0'+m;}}
 if (m==60){m='0'+0;h++;if(h<10){h='0'+h;}}
 chrono=window.setTimeout("Chrono();",1000);
