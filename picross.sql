aa3a34bd499012da8c5aa5e97547f43f86f07155
Mateo-Danton <72432264+Mateo-Danton@users.noreply.github.com>
2021-05-31 16:34:40 +0200

    update database & picross test file

--- picross.sql
+++ picross.sql
@@ -1,3 +1,9 @@
+-- MariaDB dump 10.17  Distrib 10.4.13-MariaDB, for Win64 (AMD64)
+--
+-- Host: localhost    Database: picross
+-- ------------------------------------------------------
+-- Server version	10.4.13-MariaDB
+
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
@@ -17,6 +23,7 @@ DROP TABLE IF EXISTS `grid`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `grid` (
+  `dimension` tinyint(4) NOT NULL,
   `legendC` text NOT NULL,
   `legendL` text NOT NULL,
   `solution` text NOT NULL
@@ -29,7 +36,7 @@ CREATE TABLE `grid` (
 
 LOCK TABLES `grid` WRITE;
 /*!40000 ALTER TABLE `grid` DISABLE KEYS */;
-INSERT INTO `grid` VALUES ('4,3;2,3;3,1,3;1,3;2,3;2,1,3;3,1,2;2,2;2,2;3','8;1,6;3,1,1;2;1;1,1;2;5;9;9','0011111111001011111111100010011100000000100000000010100000000000011000111110000011111111101111111110');
+INSERT INTO `grid` VALUES (15,'3,3;3;2,2;5;1,4;1,4,4;2,4,4;3,6,1,1;3,3,1,1;4,3,2,1;4,1,2;1,1,1,6;1,6,2;7,2;4,3,1\r','8;5;4,1;3,1;3;3,8;3,5,2;2,6,3;3,3;5,4;1,1,1,1,1;4,1;1,5,1;1,5,4;1,13\r','000001111111100000000111110000000000011110001000000000111001000000000000111111000011111111111001111100110110011111100111000001110000111000001111101111000100010101010000111100001000100111110001000101111100011110101111111111111');
 /*!40000 ALTER TABLE `grid` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -59,30 +66,6 @@ INSERT INTO `scoreboard10a` VALUES (3,'Xx_ProPicross_xX',60),(4,'CasualGamer',75
 UNLOCK TABLES;
 
 --
--- Table structure for table `scoreboard10b`
---
-
-DROP TABLE IF EXISTS `scoreboard10b`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `scoreboard10b` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(20) NOT NULL,
-  `time` smallint(6) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `scoreboard10b`
---
-
-LOCK TABLES `scoreboard10b` WRITE;
-/*!40000 ALTER TABLE `scoreboard10b` DISABLE KEYS */;
-/*!40000 ALTER TABLE `scoreboard10b` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
 -- Table structure for table `scoreboard15a`
 --
 
@@ -91,10 +74,10 @@ DROP TABLE IF EXISTS `scoreboard15a`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `scoreboard15a` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(20) NOT NULL,
-  `time` smallint(6) NOT NULL,
+  `nom` varchar(20) NOT NULL,
+  `temps` smallint(6) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -103,55 +86,8 @@ CREATE TABLE `scoreboard15a` (
 
 LOCK TABLES `scoreboard15a` WRITE;
 /*!40000 ALTER TABLE `scoreboard15a` DISABLE KEYS */;
+INSERT INTO `scoreboard15a` VALUES (1,'PicrossMaster',120),(2,'NoNoGrammer',160);
 /*!40000 ALTER TABLE `scoreboard15a` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `scoreboard15b`
---
-
-DROP TABLE IF EXISTS `scoreboard15b`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `scoreboard15b` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(20) NOT NULL,
-  `time` smallint(6) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `scoreboard15b`
---
-
-LOCK TABLES `scoreboard15b` WRITE;
-/*!40000 ALTER TABLE `scoreboard15b` DISABLE KEYS */;
-/*!40000 ALTER TABLE `scoreboard15b` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `scoreboard15c`
---
-
-DROP TABLE IF EXISTS `scoreboard15c`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `scoreboard15c` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(20) NOT NULL,
-  `time` smallint(6) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `scoreboard15c`
---
-
-LOCK TABLES `scoreboard15c` WRITE;
-/*!40000 ALTER TABLE `scoreboard15c` DISABLE KEYS */;
-/*!40000 ALTER TABLE `scoreboard15c` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -166,7 +102,7 @@ CREATE TABLE `scoreboard20a` (
   `name` varchar(20) NOT NULL,
   `time` smallint(6) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -175,55 +111,8 @@ CREATE TABLE `scoreboard20a` (
 
 LOCK TABLES `scoreboard20a` WRITE;
 /*!40000 ALTER TABLE `scoreboard20a` DISABLE KEYS */;
+INSERT INTO `scoreboard20a` VALUES (1,'Xx_PiX_xX',180),(2,'ProGamer',230);
 /*!40000 ALTER TABLE `scoreboard20a` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `scoreboard20b`
---
-
-DROP TABLE IF EXISTS `scoreboard20b`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `scoreboard20b` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(20) NOT NULL,
-  `time` smallint(6) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `scoreboard20b`
---
-
-LOCK TABLES `scoreboard20b` WRITE;
-/*!40000 ALTER TABLE `scoreboard20b` DISABLE KEYS */;
-/*!40000 ALTER TABLE `scoreboard20b` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `scoreboard20c`
---
-
-DROP TABLE IF EXISTS `scoreboard20c`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `scoreboard20c` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(20) NOT NULL,
-  `time` smallint(6) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `scoreboard20c`
---
-
-LOCK TABLES `scoreboard20c` WRITE;
-/*!40000 ALTER TABLE `scoreboard20c` DISABLE KEYS */;
-/*!40000 ALTER TABLE `scoreboard20c` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -234,3 +123,5 @@ UNLOCK TABLES;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-05-31 16:32:50
