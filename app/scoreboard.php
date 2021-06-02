7cf46043b843283c1b76ce9ba6d99f1264096471
Mateo-Danton <72432264+Mateo-Danton@users.noreply.github.com>
2021-06-02 11:34:06 +0200

    updated scoreboard
    updated scoreboard with fully active database link

--- app/scoreboard.php
+++ app/scoreboard.php
@@ -1,21 +1,28 @@
 <?php 
     require "game.php";
+    $nom1 = $_COOKIE['nom'];
+    $temps1 = $_COOKIE['temps'];
     switch($dimension){
     case "10":
         // Preparing the selection
         $req = $bdd->prepare('SELECT nom, temps FROM scoreboard10a ORDER BY temps');
+        $sel = $bdd->prepare("INSERT INTO scoreboard10a (nom, temps) VALUES ('$nom1', '$temps1')");
         break;
     case "15":
         // Preparing the selection
         $req = $bdd->prepare('SELECT nom, temps FROM scoreboard15a ORDER BY temps');
+        $sel = $bdd->prepare("INSERT INTO scoreboard15a (nom, temps) VALUES ('$nom1', '$temps1')");
         break;
     case "20":
         // Preparing the selection
         $req = $bdd->prepare('SELECT nom, temps FROM scoreboard20a ORDER BY temps');
+        $sel = $bdd->prepare("INSERT INTO scoreboard20a (nom, temps) VALUES ('$nom1', '$temps1')");
         break;
     }
 // Execution
+$sel->execute();
 $req->execute();
+
 // Recovery of the data
 $data = $req->fetchAll(); 
 ?>
