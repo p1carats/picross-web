54fe401b57ae724824481ce5af79b84281c54bdd
Mateo-Danton <72432264+Mateo-Danton@users.noreply.github.com>
2021-06-03 19:51:30 +0200

    Updated executable, database and pages
    Major update: updated the generator/solver executable, updated the database, and corrected/updated a lot of the pages

--- picross.sql
+++ picross.sql
@@ -24,8 +24,8 @@ DROP TABLE IF EXISTS `grid`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `grid` (
   `dimension` tinyint(4) NOT NULL,
-  `legendC` text NOT NULL,
   `legendL` text NOT NULL,
+  `legendC` text NOT NULL,
   `solution` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -36,7 +36,7 @@ CREATE TABLE `grid` (
 
 LOCK TABLES `grid` WRITE;
 /*!40000 ALTER TABLE `grid` DISABLE KEYS */;
-INSERT INTO `grid` VALUES (15,'3,3;3;2,2;5;1,4;1,4,4;2,4,4;3,6,1,1;3,3,1,1;4,3,2,1;4,1,2;1,1,1,6;1,6,2;7,2;4,3,1\r','8;5;4,1;3,1;3;3,8;3,5,2;2,6,3;3,3;5,4;1,1,1,1,1;4,1;1,5,1;1,5,4;1,13\r','000001111111100000000111110000000000011110001000000000111001000000000000111111000011111111111001111100110110011111100111000001110000111000001111101111000100010101010000111100001000100111110001000101111100011110101111111111111');
+INSERT INTO `grid` VALUES (10,'9;1,3,2;5,1,2;3,6;4,2;3,1,2;2,3,1;2,6;2,4,2;2,3\r','4,4;1,8;6,1;3,1,3;5,3;1,1,4;5,2;2,3,1,1;1,2,1,3;2,1,2\r','1111111110101110110011111010111110111111011110110011100101101101110001110111111011011110110110000111');
 /*!40000 ALTER TABLE `grid` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -52,7 +52,7 @@ CREATE TABLE `scoreboard10a` (
   `nom` varchar(20) NOT NULL,
   `temps` smallint(6) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -61,7 +61,7 @@ CREATE TABLE `scoreboard10a` (
 
 LOCK TABLES `scoreboard10a` WRITE;
 /*!40000 ALTER TABLE `scoreboard10a` DISABLE KEYS */;
-INSERT INTO `scoreboard10a` VALUES (3,'Xx_ProPicross_xX',60),(4,'CasualGamer',75),(5,'UncasualGamer',85),(6,'TheNonogramMaster',90),(7,'PuzzleEnjoyer02',105),(8,'PuzzleFan1998',120);
+INSERT INTO `scoreboard10a` VALUES (3,'Xx_ProPicross_xX',60),(4,'CasualGamer',75),(5,'UncasualGamer',85),(6,'TheNonogramMaster',90),(7,'PuzzleEnjoyer02',105),(8,'PuzzleFan1998',120),(19,'Xx_D4rKRand0m_xX',334),(20,'Xx_D4rKRand0m_xX',153);
 /*!40000 ALTER TABLE `scoreboard10a` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -77,7 +77,7 @@ CREATE TABLE `scoreboard15a` (
   `nom` varchar(20) NOT NULL,
   `temps` smallint(6) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=30 DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -86,33 +86,33 @@ CREATE TABLE `scoreboard15a` (
 
 LOCK TABLES `scoreboard15a` WRITE;
 /*!40000 ALTER TABLE `scoreboard15a` DISABLE KEYS */;
-INSERT INTO `scoreboard15a` VALUES (1,'PicrossMaster',120),(2,'NoNoGrammer',160);
+INSERT INTO `scoreboard15a` VALUES (1,'PicrossMaster',120),(2,'NoNoGrammer',160),(29,'Oats',774);
 /*!40000 ALTER TABLE `scoreboard15a` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Table structure for table `scoreboard20a`
+-- Table structure for table `scoreboard5a`
 --
 
-DROP TABLE IF EXISTS `scoreboard20a`;
+DROP TABLE IF EXISTS `scoreboard5a`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `scoreboard20a` (
+CREATE TABLE `scoreboard5a` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(20) NOT NULL,
-  `time` smallint(6) NOT NULL,
+  `nom` varchar(20) NOT NULL,
+  `temps` smallint(6) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=42 DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `scoreboard20a`
+-- Dumping data for table `scoreboard5a`
 --
 
-LOCK TABLES `scoreboard20a` WRITE;
-/*!40000 ALTER TABLE `scoreboard20a` DISABLE KEYS */;
-INSERT INTO `scoreboard20a` VALUES (1,'Xx_PiX_xX',180),(2,'ProGamer',230);
-/*!40000 ALTER TABLE `scoreboard20a` ENABLE KEYS */;
+LOCK TABLES `scoreboard5a` WRITE;
+/*!40000 ALTER TABLE `scoreboard5a` DISABLE KEYS */;
+INSERT INTO `scoreboard5a` VALUES (3,'Xx_ProPicross_xX',3),(4,'CasualGamer',15),(5,'UncasualGamer',20),(6,'TheNonogramMaster',30),(7,'PuzzleEnjoyer02',55),(8,'PuzzleFan1998',100),(19,'Oetam',6);
+/*!40000 ALTER TABLE `scoreboard5a` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -124,4 +124,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-05-31 16:32:50
+-- Dump completed on 2021-06-03 19:49:54
